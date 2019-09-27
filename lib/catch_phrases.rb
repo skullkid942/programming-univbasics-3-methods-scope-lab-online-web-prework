f251d9962a29226a654c3ee0899252c53f54a597
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-27 13:57:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,8 @@
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
+
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
     phrase = "It's-a me, Mario!"
