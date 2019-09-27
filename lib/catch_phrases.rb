dd8922d5d7959eeae1a65dcc04ec5d122fa8fbfc
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-27 13:55:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,8 @@
-def mario
-  phrase = "It's a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
 
 def toadstool
